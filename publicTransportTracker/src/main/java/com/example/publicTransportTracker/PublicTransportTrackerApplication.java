72365cd883786795b5b75c3f984275f9a17debed
Umang <umangbasuthkar51@gmail.com>
2025-04-19 18:46:21 +0530

    Initial commit: Angular and Spring Boot setup

---
+++ publicTransportTracker/src/main/java/com/example/publicTransportTracker/PublicTransportTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.publicTransportTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicTransportTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicTransportTrackerApplication.class, args);
+	}
+
+}
---
+++ publicTransportTracker/src/test/java/com/example/publicTransportTracker/PublicTransportTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.publicTransportTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicTransportTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
